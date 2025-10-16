04c62403dc85eb327f21ce8a9b1f97706346718b
Abir Chodha <abirchodha1@gmail.com>
2025-10-16 21:40:23 +0530

    Setup error middlewares

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 require("dotenv").config();
+require("express-async-errors");
+
 const express = require("express");
 const app = express();
 
 // database
 const connectDB = require("./db/connect");
 
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h1>API</h1>");
 });
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
