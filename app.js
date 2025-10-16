456ee361bd506312b47304cd486bee3f25c6f8f1
Abir Chodha <abirchodha1@gmail.com>
2025-10-16 21:33:18 +0530

    Setup basic express server

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+// database
+const connectDB = require("./db/connect");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>API</h1>");
+});
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening at port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
