846a585d302b4bf5ca25fb2a30eb6946c756cdbb
Abir Chodha <abirchodha1@gmail.com>
2025-10-18 01:11:57 +0530

    Attach jwt to cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,7 +26,14 @@ const registerController = async (req, res) => {
 
   const token = createJWT({ payload: tokenUser });
 
-  res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+  });
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 
 const loginController = async (req, res) => {
