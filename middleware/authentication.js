1deee19f2778ddf89e3ae71821483cb51f53ca8f
Abir Chodha <abirchodha1@gmail.com>
2025-10-18 19:34:51 +0530

    Verify the token and extract payload (user) from it

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -5,12 +5,29 @@ const authenticateUser = async (req, res, next) => {
   const token = req.signedCookies.token;
 
   if (!token) {
-    console.log("no token present");
-  } else {
-    console.log("token present");
+    throw new CustomError.UnauthenticatedError("Authentication Invalid");
   }
 
-  next();
+  try {
+    // isTokenValid returns the payload that we signed
+    // Remeber, we used a tokenUser as a payload
+    // Therefore, we are getting the signed in user in our payload
+    const payload = isTokenValid({ token });
+    console.log(payload);
+    /* Sampple log: 
+      (below are the details of the logged in user accessing our route)
+      {
+        userId: '68f34a1eae9576f644d10841',
+        userName: 'steve', 
+        userRole: 'user',
+        iat: 1760795227,
+        exp: 1763387227
+      }
+    */
+    next();
+  } catch (error) {
+    throw new CustomError.UnauthenticatedError("Authentication Invalid");
+  }
 };
 
 module.exports = {
