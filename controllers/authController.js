706400575a32359a4b778a30eff8711078a5a2bf
Abir Chodha <abirchodha1@gmail.com>
2025-10-18 13:48:32 +0530

    Implement logout by removing cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -59,7 +59,12 @@ const loginController = async (req, res) => {
 };
 
 const logoutController = async (req, res) => {
-  res.send("Logout User");
+  // remove cookie from the browser
+  res.cookie("token", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.status(StatusCodes.OK).json({ msg: "user logged out" });
 };
 
 module.exports = {
