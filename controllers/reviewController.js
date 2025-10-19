a586a1bece5a7d6d6ce2e2a7b83030538147db59
Abir Chodha <abirchodha1@gmail.com>
2025-10-20 00:24:04 +0530

    Implement create review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,32 @@
+const Review = require("../models/Review");
+const Product = require("../models/Product");
+const CustomError = require("../errors");
+const { StatusCodes } = require("http-status-codes");
+const { checkPermissions } = require("../utils");
+
 const createReview = async (req, res) => {
-  res.send("create review");
+  const { product: productId } = req.body;
+
+  const isValidProduct = await Product.findOne({ _id: productId });
+  if (!isValidProduct) {
+    throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+  }
+
+  const alreadySubmittedReview = await Review.findOne({
+    product: productId,
+    user: req.user.userId,
+  });
+
+  if (alreadySubmittedReview) {
+    throw new CustomError.BadRequestError(
+      "Already submitted review for this product"
+    );
+  }
+
+  req.body.user = req.user.userId;
+
+  const review = await Review.create(req.body);
+  res.status(StatusCodes.CREATED).json({ review });
 };
 
 const getAllReviews = async (req, res) => {
