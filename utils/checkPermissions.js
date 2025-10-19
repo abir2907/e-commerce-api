8ed041cc0eb6cf075f2b699a639e8b8caf8ae24c
Abir Chodha <abirchodha1@gmail.com>
2025-10-19 15:09:05 +0530

    Finis implementing checkPermissions func

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -4,22 +4,11 @@ const checkPermissions = (requestUser, resourceUserId) => {
   // If the user making the getSignleUser request is not an admin,
   // Then we don't want that user to see other users using their id
   // Admin can get every user, but the user should only be able to see himself using his id, and not anyone else
-  console.log(`RequestUserId: ${requestUser.userId}`);
-  console.log(`ResourceUserId: ${resourceUserId}`);
-  console.log(`Type of requestUserId: ${typeof requestUser.userId}`);
-  console.log(`Type of resourceUserId: ${typeof resourceUserId}`);
-  // Sample log:
-  /* 
-     RequestUserId: 68f34a1eae9576f644d10841
-     ResourceUserId: 68f395cac6ce9da0b85eef3d
-     Type of requestUserId: string
-     Type of resourceUserId: object
 
-     Notice that the user ids are different, therefore someone else is trying to access some other user. It it's admin, it's ok, but if it's a user, then we should throw an error
+  if (requestUser.role === "admin") return;
+  if (requestUser.userId === resourceUserId.toString()) return;
 
-     Also notice that the type of req.user.userId is String but 
-     user._id is object
-  */
+  throw new CustomError.UnauthorizedError("Not authorized to acess this route");
 };
 
 module.exports = checkPermissions;
