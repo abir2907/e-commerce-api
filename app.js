ffe15fa739534260ce2af346f3b84d21d10530eb
Abir Chodha <abirchodha1@gmail.com>
2025-10-21 21:52:02 +0530

    Add security packages

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 require("express-async-errors");
 
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
+
 const express = require("express");
 const app = express();
 
@@ -26,6 +33,17 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use(express.static("./public"));
 app.use(fileUpload());
